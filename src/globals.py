4531d9550e6dbcb77d5d8c200270e2248ebd0610
cxnt <pavelbartsits@gmail.com>
2022-11-24 15:58:35 +0400

    update env file

--- src/globals.py
+++ src/globals.py
@@ -1,14 +1,9 @@
 import os
 import sys
 from distutils.util import strtobool
-from pathlib import Path
 
 import supervisely as sly
 from dotenv import load_dotenv
-
-root_source_path = str(Path(sys.argv[0]).parents[2])
-sly.logger.info(f"Root source directory: {root_source_path}")
-sys.path.append(root_source_path)
 
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
